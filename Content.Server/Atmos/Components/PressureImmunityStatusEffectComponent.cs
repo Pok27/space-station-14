438da19f4d75b5a8af0f4c0cf92500e58d78b4ca
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-04-20 17:15:38 +0300

    fix

--- Content.Server/Atmos/Components/PressureImmunityStatusEffectComponent.cs
+++ Content.Server/Atmos/Components/PressureImmunityStatusEffectComponent.cs
@@ -4,6 +4,4 @@ namespace Content.Server.Atmos.Components;
 /// Grants immunity to barotrauma while present either as a direct component or as a status effect.
 /// </summary>
 [RegisterComponent]
-public sealed partial class PressureImmunityStatusEffectComponent : Component
-{
-}
+public sealed partial class PressureImmunityStatusEffectComponent : Component;
