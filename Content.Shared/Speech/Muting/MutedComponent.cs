faf513ea34f3e305ff7637b6409b80bbf0d05669
Pok245 <113675512+Pok27@users.noreply.github.com>
2026-04-20 13:50:12 +0300

    MutedStatusEffects

--- Content.Shared/Speech/Muting/MutedComponent.cs
+++ Content.Shared/Speech/Muting/MutedComponent.cs
@@ -1,10 +1,9 @@
 using Robust.Shared.GameStates;
 
-namespace Content.Shared.Speech.Muting
-{
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class MutedComponent : Component
-    {
+namespace Content.Shared.Speech.Muting;
 
-    }
-}
+/// <summary>
+/// Prevents an entity from speaking, screaming, and producing vocal emotes.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MutedComponent : Component;
